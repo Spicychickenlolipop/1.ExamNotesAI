c817b8002b2b55ed6f5009ee064ba26ce6e5d0d7
Anirudra Kisku <kisku.anirudra@gmail.com>
2026-03-11 03:34:43 +0530

    1st commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDb from './utils/connectDb.js';
+// import { auth } from '../client/src/utils/firebase.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRouter from './routes/auth.route.js';
+// import { use } from 'react';
+import userRouter from './routes/user.route.js';
+dotenv.config();
+
+
+const app = express()
+
+app.use(cors(
+    {origin:"http://localhost:5173",
+        credentials:true,
+        methods:["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+    }
+))
+app.use(express.json());
+app.use(cookieParser());
+
+
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.json({message: 'ExamNotesAI backend is running!'});
+});
+app.use("/api/auth",authRouter)
+app.use("/api/user",userRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDb();
+});
